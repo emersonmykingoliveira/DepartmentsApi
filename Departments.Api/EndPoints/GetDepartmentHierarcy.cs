e0f6b7d1fbad93295084be372b9d5143eada3786
Emerson de Oliveira <Emerson.deOliveira@vitecsoftware.com>
2025-10-11 19:52:16 +0200

    Adding GetDepartment endpoint

---
+++ Departments.Api/EndPoints/GetDepartmentHierarcy.cs
@@ -0,0 +1,10 @@
+﻿namespace Departments.Api.EndPoints
+{
+    public static partial class Endpoint
+    {
+        public static void GetDepartmentsHierarcy(this WebApplication app)
+        {
+
+        }
+    }
+}
