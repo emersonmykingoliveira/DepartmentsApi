f02a60ec585650f9f5bc623924795e5229f1d8cd
Emerson de Oliveira <Emerson.deOliveira@vitecsoftware.com>
2025-10-12 12:33:25 +0200

    Moving to controllers

---
+++ Departments.Api/Controllers/DepartmentController.cs
@@ -0,0 +1,27 @@
+﻿using Departments.BusinessLayer.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Departments.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DepartmentsController : ControllerBase
+    {
+        private readonly IDepartmentFileReaderService _departmentService;
+        private readonly IConfiguration _config;
+
+        public DepartmentsController(IDepartmentFileReaderService departmentService, IConfiguration config)
+        {
+            _departmentService = departmentService;
+            _config = config;
+        }
+
+        [HttpGet("hierarchy")]
+        public async Task<IActionResult> GetHierarchy()
+        {
+            string filePath = _config["DepartmentFiles:Path"] ?? string.Empty;
+            var result = await _departmentService.ReadAllFilesAsync(filePath);
+            return Ok(result);
+        }
+    }
+}
