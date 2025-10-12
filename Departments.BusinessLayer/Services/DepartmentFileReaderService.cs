e53c058c9f9adb11690c12022c66a533c361f040
Emerson de Oliveira <Emerson.deOliveira@vitecsoftware.com>
2025-10-12 19:41:06 +0200

    Spliting classes

--- Departments.BusinessLayer/Services/DepartmentFileReaderService.cs
+++ Departments.BusinessLayer/Services/DepartmentFileReaderService.cs
@@ -1,122 +1,35 @@
 ﻿using Departments.BusinessLayer.Models;
-using System;
-using System.Collections.Generic;
-using System.IO;
 using System.IO.Abstractions;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Departments.BusinessLayer.Services
 {
     public class DepartmentFileReaderService : IDepartmentFileReaderService
     {
         private readonly IFileSystem _fileSystem;
+        private readonly IDepartmentParser _parser;
+        private readonly IDepartmentHierarchyBuilder _hierarchyBuilder;
 
-        public DepartmentFileReaderService(IFileSystem fileSystem)
+        public DepartmentFileReaderService(
+            IFileSystem fileSystem,
+            IDepartmentParser parser,
+            IDepartmentHierarchyBuilder hierarchyBuilder)
         {
             _fileSystem = fileSystem;
+            _parser = parser;
+            _hierarchyBuilder = hierarchyBuilder;
         }
 
         public async Task<List<Department>> ReadAllFilesAsync(string directoryPath)
         {
             var allDepartments = new List<Department>();
 
-            foreach (var file in Directory.EnumerateFiles(directoryPath))
+            foreach (var file in _fileSystem.Directory.EnumerateFiles(directoryPath))
             {
-                var departments = await ReadFileAsDepartmentsAsync(file);
-                if (departments?.Count > 0)
-                    allDepartments.AddRange(departments);
+                var departments = await _parser.ParseFileAsync(file);
+                allDepartments.AddRange(departments);
             }
 
-            return BuildDepartmentHierarchy(allDepartments);
-
-        }
-
-        private async Task<List<Department>> ReadFileAsDepartmentsAsync(string filePath)
-        {
-            var departments = new List<Department>();
-
-            string[] lines = await _fileSystem.File.ReadAllLinesAsync(filePath);
-
-            int lineNumber = 0;
-
-            foreach (var line in lines.Skip(1))
-            {
-                lineNumber++;
-
-                if (string.IsNullOrWhiteSpace(line)) continue;
-
-                if (TryParseDepartment(line, lineNumber) is { } department)
-                    departments.Add(department);
-            }
-
-            return departments;
-        }
-
-        private List<Department> BuildDepartmentHierarchy(List<Department> departments)
-        {
-            var dict = BuildDepartmentsDictionary(departments);
-            var roots = new List<Department>();
-
-            foreach (var dept in departments)
-            {
-                if (dict.TryGetValue(dept.DepartmentParentOID, out var parent))
-                    parent.Departments.Add(dept);
-                else
-                    roots.Add(dept);
-            }
-
-            return roots;
-        }
-
-
-        private Dictionary<int, Department> BuildDepartmentsDictionary(List<Department> departmentsCollection)
-        {
-            Dictionary<int, Department> departmentDictionary = new Dictionary<int, Department>();
-
-            foreach (var department in departmentsCollection)
-            {
-                departmentDictionary.TryAdd(department.Oid, department);
-            }
-
-            return departmentDictionary;
-        }
-
-        private Department TryParseDepartment(string line, int lineNumber)
-        {
-            var content = line.Split(',');
-
-            Department departmentWithParent = new Department();
-
-            //OID
-            if (int.TryParse(content[0], out int oID))
-                departmentWithParent.Oid = oID;
-            else
-                throw new ArgumentException($"Line number {lineNumber}, error parsing OID");
-
-            //Title
-            if (string.IsNullOrWhiteSpace(content[1]))
-                throw new ArgumentException($"Line number {lineNumber}, error parsing Title");
-
-            departmentWithParent.Title = content[1];
-
-            //Color
-            if (string.IsNullOrWhiteSpace(content[2]))
-                throw new ArgumentException($"Line number {lineNumber}, error parsing Color");
-
-            departmentWithParent.Color = content[2];
-
-            //DepartmentParentOID
-            if (!string.IsNullOrWhiteSpace(content[3]))
-            {
-                if (int.TryParse(content[3], out int departmentParentOID))
-                    departmentWithParent.DepartmentParentOID = departmentParentOID;
-                else
-                    throw new ArgumentException($"Line number {lineNumber}, error parsing DepartmentParentOID");
-            }
-
-            return departmentWithParent;
+            return _hierarchyBuilder.BuildHierarchy(allDepartments);
         }
     }
 }
