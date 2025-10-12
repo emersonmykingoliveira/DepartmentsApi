4bf3fb093cc1fd61c532e9271d3df906d536d42b
Emerson de Oliveira <Emerson.deOliveira@vitecsoftware.com>
2025-10-12 19:48:40 +0200

    Adding DepartmentHierarchyBuilder implements IDepartmentHierarchyBuilder

---
+++ Departments.BusinessLayer/Services/DepartmentHierarchyBuilder.cs
@@ -0,0 +1,36 @@
+﻿using Departments.BusinessLayer.Models;
+using System.Collections.Generic;
+
+namespace Departments.BusinessLayer.Services
+{
+    public class DepartmentHierarchyBuilder : IDepartmentHierarchyBuilder
+    {
+        public List<Department> BuildDepartmentHierarchy(List<Department> departments)
+        {
+            var dict = BuildDepartmentsDictionary(departments);
+            var roots = new List<Department>();
+
+            foreach (var dept in departments)
+            {
+                if (dict.TryGetValue(dept.DepartmentParentOID, out var parent))
+                    parent.Departments.Add(dept);
+                else
+                    roots.Add(dept);
+            }
+
+            return roots;
+        }
+
+        private Dictionary<int, Department> BuildDepartmentsDictionary(List<Department> departmentsCollection)
+        {
+            Dictionary<int, Department> departmentDictionary = new Dictionary<int, Department>();
+
+            foreach (var department in departmentsCollection)
+            {
+                departmentDictionary.TryAdd(department.Oid, department);
+            }
+
+            return departmentDictionary;
+        }
+    }
+}
