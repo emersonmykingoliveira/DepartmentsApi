93b4302aa158bc1f6089dd7c5c98df9d12f70a7f
Emerson de Oliveira <Emerson.deOliveira@vitecsoftware.com>
2025-10-12 19:47:49 +0200

    Adding interface IDepartmentHierarchyBuilder

---
+++ Departments.BusinessLayer/Services/IDepartmentHierarchyBuilder.cs
@@ -0,0 +1,10 @@
+﻿using Departments.BusinessLayer.Models;
+using System.Collections.Generic;
+
+namespace Departments.BusinessLayer.Services
+{
+    public interface IDepartmentHierarchyBuilder
+    {
+        List<Department> BuildDepartmentHierarchy(List<Department> departments);
+    }
+}
