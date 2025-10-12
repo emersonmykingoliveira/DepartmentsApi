706aa81d8cd28336b283bab4d66c784f80436728
Emerson de Oliveira <Emerson.deOliveira@vitecsoftware.com>
2025-10-12 09:42:24 +0200

    Add Middleware

---
+++ Departments.Api/Middleware/FileExceptionMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json;
+
+namespace Departments.Api.Middleware
+{
+    public sealed class FileExceptionMiddleware
+    {
+        private readonly RequestDelegate _requestDelegate;
+        private readonly ILogger<FileExceptionMiddleware> _logger;
+
+        public FileExceptionMiddleware(RequestDelegate requestDelegate, ILogger<FileExceptionMiddleware> logger)
+        {
+            _requestDelegate = requestDelegate;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _requestDelegate(httpContext);
+            }
+            catch (Exception ex)
+            {
+
+                var problem = new
+                {
+                    type = "https://tools.ietf.org/html/rfc7807",
+                    title = "Invalid JSON",
+                    detail = ex.Message,
+                    status = 400
+                };
+
+                await JsonSerializer.SerializeAsync(httpContext.Response.Body, problem);
+            }
+        }
+    }
+}
